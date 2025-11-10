89cc4fdbb5a55ab4af2e31f092cca2c0e177b96e
AmyWebb16 <c24423456@myTUDublin.ie>
2025-11-10 09:15:53 +0000

    Changes to be committed: added mysql to page

--- assignment1/MainPage.php
+++ assignment1/MainPage.php
@@ -6,7 +6,24 @@
     <link rel="stylesheet" href="styles/MainPage.css">
 </head>
 <body>
+    <?php
+        $servername = "localhost";
+        $username = "root";
+        $password = "";
+        $dbname = "library1_db";
+
+        $conn = new mysqli($servername, $username, $password, $dbname);
+
+        if ($conn->connect_error) {
+            die("Connection failed: " . $conn->connect_error);
+        }
+        echo "Connected successfully";
+        ?>
+        
+        
     <h1>Online Library</h1>
+
+    <h2>Login</h2>
 
     
 </body>
